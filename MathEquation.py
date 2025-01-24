26d7c55148ea9485c51e4b77fb66ae1411917c12
b-atif <atif.b@northeastern.edu>
2025-01-24 14:42:40 +0000

    MathEquation.py

--- MathEquation.py
+++ MathEquation.py
@@ -4,23 +4,24 @@ import random
 
 def generate_equation():
     num_1 = random.randint(1, 9)  # Generate a random integer between 1 and 9 for the first number
-    answer = random.randint(10,20)
+    answer = random.randint(10,20) # Generate a random integer between 10 and 20 for what the eqn is equivalent to
     X = answer/num_1
     return num_1, X, answer, 
 
 score = 0
 
+# Generates a random equation five times
 for _ in range(5):
     num_1, X, answer = generate_equation()
     message = "Solve the equation " + str(num_1) + " * " + '𝑥 ' + ' = ' + str(answer)
     print(message)
     user_answer = float(input("Your answer for X (1 dp if required) : "))
-    user_answer = round(user_answer, 1)
-    X = round(X,1)
+    user_answer = round(user_answer, 1) 
+    X = round(X,1) # Ensures input can handle to 1 decimal place
 
-    if user_answer == X:
+    if user_answer == X: # Compares if users answer is equal to X
         print("Correct!")
-        score += 1
+        score += 1 # Adds up the score
     else:
         wrong_answer_message = "Wrong. The correct answer was: " + str(X)
         print(wrong_answer_message)
